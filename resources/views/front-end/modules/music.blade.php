6180969bb10740249ccd71696d084810697b3f60
= <=>
2019-11-18 22:56:57 +0700

    font-end

---
+++ resources/views/front-end/modules/music.blade.php
@@ -0,0 +1,85 @@
+@extends('front-end.layout.master')
+@section('pageTitle', 'BKSTORE:Shop')
+@section('content')
+<div class="main">
+    <div class="container">
+    <div class="category">
+                    <div class="title">MUSIC</div>
+                    <div class="dash"></div>
+                    <div class="content">
+                        <div class="component-ticket">
+                            <div class="img item-3"></div> 
+                            <div class="text">
+                                <div class="price">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="padding-10">
+                                                <div class="table w-100 margin-bottom-0">
+                                                    <div class="table-cell event-title">
+                                                        <a href="https://ticketbox.vn/event/chay-vi-trai-tim-run-for-the-heart-2019-77019?source=home_hot_1"
+                                                            title="CHẠY VÌ TRÁI TIM / RUN FOR THE HEART 2019" target="_blank">
+                                                            Hòa nhạc “Bản Giao hưởng Bi thương” - The Dramatic Pathétique
+                                                        </a>
+                                                    </div>
+                                                    <div class="table-cell card-right-block">
+                                                    </div>
+                                                </div>
+                                                <div class="table w-100 margin-bottom-0">
+                                                    <div class="table-cell">
+                                                        <div class="event-price w-100">
+                                                            <span class="color-6">Từ</span> <strong> 300,000 VNĐ</strong>
+                                                        </div>
+                                                        <div class="event-tags w-100">
+                                                            <div class="tag-venues">
+                                                                <span class="tag-venue smooth-trans  uppercase">Hà Nội</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="component-ticket">
+                            <div class="img item-4"></div> 
+                            <div class="text">
+                                <div class="price">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="padding-10">
+                                                <div class="table w-100 margin-bottom-0">
+                                                    <div class="table-cell event-title">
+                                                        <a href="https://ticketbox.vn/event/chay-vi-trai-tim-run-for-the-heart-2019-77019?source=home_hot_1"
+                                                            title="CHẠY VÌ TRÁI TIM / RUN FOR THE HEART 2019" target="_blank">
+                                                            Đêm nhạc: Gã lang thang ngồi hát tình tang
+                                                        </a>
+                                                    </div>
+                                                    <div class="table-cell card-right-block">
+                                                    </div>
+                                                </div>
+                                                <div class="table w-100 margin-bottom-0">
+                                                    <div class="table-cell">
+                                                        <div class="event-price w-100">
+                                                            <span class="color-6">Từ</span> <strong> 169,000 VNĐ</strong>
+                                                        </div>
+                                                        <div class="event-tags w-100">
+                                                            <div class="tag-venues">
+                                                                <span class="tag-venue smooth-trans  uppercase">Hồ Chí Minh</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+    </div>
+</div>
+@endsection('content')
