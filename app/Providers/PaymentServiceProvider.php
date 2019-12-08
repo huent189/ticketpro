3f07528f7b24f1c89509720df2022ab72ebd18fd
huent189 <hueahgase189@gmail.com>
2019-12-08 14:49:17 +0700

    added momo payment (not tested yet)

--- app/Providers/PaymentServiceProvider.php
+++ app/Providers/PaymentServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\PaymentGateway\Payment;
+use MService\Payment\Shared\SharedModels\Environment;
+use MService\Payment\Shared\SharedModels\PartnerInfo;
 
 class PaymentServiceProvider extends ServiceProvider
 {
@@ -14,9 +16,15 @@ class PaymentServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(Payment::class, function ($app)
+        // $this->app->bind(Payment::class, function ($app)
+        // {
+        //     return new Payment();
+        // });
+        $this->app->bind(Environment::class, function ($app)
         {
-            return new Payment();
+            return new Environment(env('DEV_MOMO_ENDPOINT'), 
+                                new PartnerInfo(env('DEV_ACCESS_KEY'), env('DEV_PARTNER_CODE'), env('DEV_SECRET_KEY')),
+                                env('DEV'));   
         });
     }
 
--- app/Services/PaymentGateway/Payment.php
+++ app/Services/PaymentGateway/Payment.php
@@ -1,61 +1,31 @@
 <?php
 
-namespace App\Services\PaymentGateway;
 
+namespace App\Services\PaymentGateway;
 use Illuminate\Support\Facades\URL;
+use MService\Payment\AllInOne\Processors\CaptureIPN;
+use MService\Payment\AllInOne\Processors\CaptureMoMo;
+use MService\Payment\Shared\SharedModels\Environment;
 
 class Payment
 {
-    public function purchase($bookingId, $orderInfo, $amount, $clientIp)
+    protected $env;
+    public function __construct(Environment $e) {
+        $this->env = $e;
+    }
+    public function purchase($bookingId, $orderInfo, string $amount, $clientIp)
     {
-        $vnp_HashSecret = env('VNP_CHECKSUM'); //Chuỗi bí mật
-        $vnp_OrderType = 'topup';
-        $vnp_Amount = $amount * 100;
-        $vnp_Locale = 'vn';
-        $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        //TODO : edit this
-        $vnp_Returnurl = URL::to('booking/complete');
-        $inputData = array(
-            "vnp_Version" => "2.0.0",
-            "vnp_TmnCode" => env('VNP_TMN_CODE'),
-            "vnp_Amount" => $vnp_Amount,
-            // "vnp_BankCode" => "NCB",
-            "vnp_Command" => "pay",
-            "vnp_CreateDate" => date('YmdHis'),
-            "vnp_CurrCode" => "VND",
-            "vnp_IpAddr" => $clientIp,
-            "vnp_Locale" => $vnp_Locale,   
-            "vnp_OrderInfo" => $orderInfo,
-            "vnp_OrderType" => $vnp_OrderType,
-            "vnp_ReturnUrl" => $vnp_Returnurl,
-            "vnp_TxnRef" => $bookingId,    
-        );
-        ksort($inputData);
-        $query = "";
-        $i = 0;
-        $hashdata = "";
-        foreach ($inputData as $key => $value) {
-            if ($i == 1) {
-                $hashdata .= '&' . $key . "=" . $value;
-            } else {
-                $hashdata .= $key . "=" . $value;
-                $i = 1;
-            }
-            $query .= urlencode($key) . "=" . urlencode($value) . '&';
-        }
-
-        $vnp_Url = $vnp_Url . "?" . $query;
-        if (isset($vnp_HashSecret)) {
-            $vnpSecureHash = hash('sha256',$vnp_HashSecret . $hashdata);
-            $vnp_Url .= 'vnp_SecureHashType=SHA256&vnp_SecureHash=' . $vnpSecureHash;
-        }
-        error_log($vnp_Url);
-        return  $vnp_Url;
+        $requestId = rand(100, 999999999);
+        $notifyURL = URL::to("/checkPayment");
+        $returnURL = URL::to("/booking/".strval($bookingId)."#complete");
+        return CaptureMoMo::process($this->env,$bookingId, $orderInfo, $amount,'', $requestId, 
+                        $notifyURL, $returnURL);
+        
     }
 
-    public function receiveIPN()
+    public function receiveIPN(string $rawPostData)
     {
-        
+        return CaptureIPN::process($this->env, $rawPostData);
     }
 
 }
