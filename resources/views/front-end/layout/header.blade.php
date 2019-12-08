a0896e0a1ae76628dd610abf55ac6ecf1464e082
= <=>
2019-12-08 13:34:46 +0700

    update responsive

--- resources/views/front-end/layout/header.blade.php
+++ resources/views/front-end/layout/header.blade.php
@@ -12,7 +12,6 @@
                     <div class="component-toolbar"><a href="/sport">SPORT</a></div>
                     <div class="component-toolbar"><a href="/music">MUSIC</a></div>
                     <div class="component-toolbar"><a href="/conference">CONFERENCE</a></div>
-                    <div class="component-toolbar"><a href="#">CREATE EVENT</a></div>
                 </div>
                 <div class="search-form">
                     <input type="text" placeholder="Search..">
@@ -53,26 +52,3 @@
     </div>
 </div> <!-- End site branding area -->
 
-<!-- <div class="mainmenu-area">
-    <div class="container">
-        <div class="row">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            <div class="navbar-collapse collapse hmenu">
-                <ul class="nav navbar-nav ">
-                    <li id="homepage123"><a href="/">Trang chủ</a></li>
-                    <li id="shop"><a href="{{url('products/all')}}">Cửa hàng</a></li>
-                    <li id="cart"><a href="/gio-hang">Giỏ hàng</a></li>
-                    <li id="payment"><a href="/dat-hang">Thanh toán</a></li>
-                    <li id="blog"><a href="/blog">Tin tức</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div> End mainmenu area -->
