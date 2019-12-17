50a83615809e3b782fb99623fadc262d6aad6d40
Nguyen Huu Thanh <huuthanh3199hn@hotmail.com>
2019-12-17 18:43:24 +0700

    sửa header đăng nhập

--- resources/views/front-end/layout/master.blade.php
+++ resources/views/front-end/layout/master.blade.php
@@ -10,19 +10,19 @@
 
 
     <!-- Google Fonts -->
-    <link href='fonts\Titillium.css' rel='stylesheet' type='text/css'>
-    <link href='fonts\Roboto.css' rel='stylesheet' type='text/css'>
-    <link href='fonts\Titillium.css' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="\css\w3.css">
+    <link href='/fonts/Titillium.css' rel='stylesheet' type='text/css'>
+    <link href='/fonts/Roboto.css' rel='stylesheet' type='text/css'>
+    <link href='/fonts/Titillium.css' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/w3.css">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
 
-    <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
 
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
     <!-- Font Awesome -->
     
     <link rel="stylesheet" href="{{asset("css/font-awesome.min.css")}}">
@@ -77,7 +77,7 @@
 <!-- include js files -->
 <!-- Latest jQuery form server -->
 
-<script src="js/jquery.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 <script src="{{asset("js/jquery.min.js")}}"></script>
 
 {{-- <script src="/js/bootstrap-slider.js"></script> --}}
@@ -98,24 +98,24 @@
 {{-- <script type="text/javascript" src={{ asset('js/bxslider.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset("js/script.slider.js")}}"></script> --}}
 @stack('scripts')
-<script src="jquery\jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="bootstrap\css\bootstrap.min.css"></script>
+<script src="/jquery/jquery.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/bootstrap/css/bootstrap.min.css"></script>
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 
 
-<script src="js/html5shiv.min.js"></script>
-<script type="text/javascript" src="js/loader.js"></script>
+<script src="/js/html5shiv.min.js"></script>
+<script type="/text/javascript" src="js/loader.js"></script>
 <script src="js/respond.min.js"></script>
 <script src="/js/chart_total.js"></script>
 <script src="/js/chart_all_ticket.js"></script>
 <script src="/js/clock.js"></script>
 
 
-<script src="js/5279a6eae4.js"></script>
-<script src="jquery/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+<script src="/js/5279a6eae4.js"></script>
+<script src="/jquery/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 {{--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>--}}
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
-<script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
