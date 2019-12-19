13cb9c329de8ac6b4908b73245421c1c33228f5e
= <=>
2019-12-20 05:10:05 +0700

    chi tiet tung ve khach mua

--- resources/views/front-end/modules/buyHistoryDetail.blade.php
+++ resources/views/front-end/modules/buyHistoryDetail.blade.php
@@ -1,118 +1,53 @@
 @extends('front-end.layout.master')
+@section('pageTitle', 'TicketPro: buyHistory')
 @push('css')
-<link rel="stylesheet" href="/css/buyTicket.css">
 <link rel="stylesheet" href="/css/CreateEvent1.css">
 <link rel="stylesheet" href="/css/buyHistory.css">
 @endpush
 
-@section('pageTitle', 'TicketPro: buyTicketHistoryDetail')
 @section('content')
-<div class="main main-choose-tickets">
-
-    <div class="wrapper">
-        <div class="banner"></div>
-        <div class="title">
-            <div class="media">
-                <img src="https://picsum.photos/200/200" style="height: 100px; width: 100px;"
-                    class="align-self-start mr-3" alt="...">
-                <div class="media-body">
-                    <h5 class="mt-0">Show của Thế Anh</h5>
-                    <p>Thứ 7 Ngày 09 tháng 11 năm 2019 (08:00 PM - 11:00 PM)</p>
-                    <p>Đại học Công nghệ - Đại học Quốc gia Hà Nội</p>
-                    <p>144 Xuân Thủy,Dịch Vọng Hậu, Cầu Giấy, Hà Nội</p>
-                </div>
-            </div>
-        </div>
-        <div class="introduction">
-            <div class="media">
-                <div class="media-body">
-                    <h3 class="mt-0">Giới thiệu</h3>
-                    <div class="space"></div>
-                    Cras sit amet
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                    fringilla. Donec
-                    lacinia congue felis in faucibus.
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                    fringilla. Donec
-                    lacinia congue felis in faucibus.
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                    fringilla. Donec
-                    lacinia congue felis in faucibus.
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                    fringilla. Donec
-                    lacinia congue felis in faucibus.
-                    Cras sit amet nibh liberolum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                    vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    Cras sit amet nibh libero, in gravida nulla Nulla vel metus scelerisque ante sollicitudin. Cras
-                    purus odio,
-                    vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                    fringilla. Donec
-                    lacinia congue felis in faucibus.
-                    nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio,
-                    vestibulum in
-                    vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
-                    lacinia
-                    congue felis in faucibus.
-                </div>
-            </div>
-        </div>
-        <div class="info">
-            <div class="media">
-                <div class="media-body">
-                    <h3 class="mt-0">Thông tin vé</h3>
-                    <div class="space"></div>
-                    <div class="img-location"></div>
-                    <div class="ticket">
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>
-                                        <h5>HOOK A:</h5>
-                                        <p>Vị trí: Khán đài A</p>
-                                        <p>Qùa tặng sẽ nhận tại sự kiện: nón</p>
-                                    </td>
-                                    <td>
-                                        <h5>400000VND</h5>
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>
-                                        <h5>HOOK A:</h5>
-                                        <p>Vị trí: Khán đài A</p>
-                                        <p>Qùa tặng sẽ nhận tại sự kiện: nón</p>
-                                    </td>
-                                    <td>
-                                        <h5>400000VND</h5>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>
-                                        <h5>HOOK A:</h5>
-                                        <p>Vị trí: Khán đài A</p>
-                                        <p>Qùa tặng sẽ nhận tại sự kiện: nón</p>
-                                    </td>
-                                    <td>
-                                        <h5>400000VND</h5>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="wrapper">
+    @include('front-end.layout.menu-left-create-event')
+    <div class="right">
+    <h1>Chi tiết từng loại vé bạn đã mua</h1>
+    <table class="table table-bordered" style = "width:96%;"> 
+    <thead>
+    <tr>
+      <th scope="col">STT</th>
+      <th scope="col">Loại vé</th>
+      <th scope="col">Giá vé</th>
+      <th scope="col">Số lượng đã bán ra</th>
+      <th scope="col">Số lượng còn lại</th>
+      <th scope="col">Tổng số tiền thu được</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Hòa âm ánh sáng</td>
+      <td>400.000 VND</td>
+      <td>12</td>
+      <td>50</td>
+      <td>5.000.000 VND</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Hòa âm ánh sáng</td>
+      <td>400.000 VND</td>
+      <td>12</td>
+      <td>50</td>
+      <td>5.000.000 VND</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Hòa âm ánh sáng</td>
+      <td>400.000 VND</td>
+      <td>12</td>
+      <td>50</td>
+      <td>5.000.000 VND</td>
+    </tr>
+  </tbody>
+    </table>
     </div>
 </div>
-@endsection('content')
+@endsection
